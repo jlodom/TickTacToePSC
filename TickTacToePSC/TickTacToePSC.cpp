589ebd2dfce5e1f100082d085dfd5d236a3323d1
Johnnie Odom <jlodom00@alumni.amherst.edu>
2022-02-07 19:54:19 -0600

    Quite a bit more program but lots of stubs.

--- TickTacToePSC/TickTacToePSC.cpp
+++ TickTacToePSC/TickTacToePSC.cpp
@@ -2,12 +2,31 @@
 
 using namespace std;
 
+/* Constants (no magic number)*/
 const int EMPTY = 0;
 const int PX = 1;
 const int PY = 4;
 const int ROW = 3;
+const int MAXMOVES = 9;
 
-int WinCondition(int t, int s, int u) {
+
+// STUB FOR LATER
+void PrintBoard(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
+    cout << a << " | ";
+}
+char PP(int player) {
+    char charPlayerSymbol = '*';
+    if (player == PX) {
+        charPlayerSymbol = 'X';
+    }
+    else if (player == PY) {
+        charPlayerSymbol = 'Y';
+    }
+    return charPlayerSymbol;
+}
+
+
+int IdentifyWinner(int t, int s, int u) {
     int winner = EMPTY;
     int v = t + s + u;
     if (v == (ROW * PX)) {
@@ -20,6 +39,18 @@ int WinCondition(int t, int s, int u) {
 }
 
 
+bool WinnerExists(int t, int s, int u) {
+    bool boolWinnerExists = false;
+    int intWinnerNumber = IdentifyWinner(t, s, u);
+    if ((intWinnerNumber == PX) || (intWinnerNumber == PY)) {
+        boolWinnerExists = true;
+    }
+    else {
+        boolWinnerExists = false;
+    }
+    return boolWinnerExists;
+}
+
 int main()
 {
     /*
@@ -31,19 +62,93 @@ int main()
     h | i | j
     */
 
+    /* Win Conditions
+    
+    a,b,c
+    e,f,g
+    h,i,j
+    a,e,h
+    b,f,i
+    c,g,j
+    a,f,j
+    c,f,h
+    
+    */
 
-    int a, b, c, d, e, f, g, h, i = EMPTY; /* Mass initialization */
-    a = b = c = d = e = f = g = h = i; /* Works on ints or any time we pass by VALUE not reference.*/
+    /* Variable Declarations */
+    int intWinStatus = 0;
+    bool boolHaveWinner = false;
+    int intMoveTracker = 0;
+    int a, b, c, d, e, f, g, h, i, j = EMPTY; /* Mass initialization */
+    a = b = c = d = e = f = g = h = i = j; /* Works on ints or any time we pass by VALUE not reference.*/
+    int playerOne;
+    int playerTwo;
+    // STUB Write some randomizer code 
+    // For right now, hard-code it
+    playerOne = PX;
+    playerTwo = PY;
+    // Eliminate the coe above when randomization is done.
+
+    bool boolKeepRunning = true;
+    while (boolKeepRunning) {
+
+        int intPlayerOneMove = 0;
+
+        bool boolContinueMoveLoop = true;
+        /* Might be a good place for "do while" */
+        while (boolContinueMoveLoop) {
+            cout << "Player One, please select a move: ";
+
+            // TODO: Show the current board.
+            cin >> intPlayerOneMove;
+            // TODO: Validation Loop (Correct value for board)
+            // TODO: Check validity of move (space must be blank)
+        }
 
 
 
-    int winStatus = 0;
-    winStatus = WinCondition(a, e, i);
 
-    if (winStatus == PX) {
+        /* Check Win Condition*/
+
+        if (WinnerExists(a, b, c)) {
+            intWinStatus = IdentifyWinner(a, b, c);
+        }
+        else if (WinnerExists(e, f, g)) {
+            intWinStatus = IdentifyWinner(e, f, g);
+        }
+        else if (WinnerExists(h, i, j)) {
+            intWinStatus = IdentifyWinner(h, i, j);
+        }
+        else if (WinnerExists(a, e, h)) {
+            intWinStatus = IdentifyWinner(a, e, h);
+        }
+        else if (WinnerExists(b, f, i)) {
+            intWinStatus = IdentifyWinner(b, f, i);
+        }
+        else if (WinnerExists(c, g, j)) {
+            intWinStatus = IdentifyWinner(c, g, j);
+        }
+        else if (WinnerExists(a, f, j)) {
+            intWinStatus = IdentifyWinner(a, f, j);
+        }
+        else if (WinnerExists(c, f, h)) {
+            intWinStatus = IdentifyWinner(c, f, h);
+        }
+
+        intMoveTracker++;
+        if ((intMoveTracker > MAXMOVES) || (intWinStatus == PX) || (intWinStatus == PY))   {
+            boolKeepRunning = false;
+        }
+    }
+
+
+    
+    intWinStatus = IdentifyWinner(a, e, i);
+
+    if (intWinStatus == PX) {
         cout << "X Has Won";
     }
-    else if (winStatus == PY) {
+    else if (intWinStatus == PY) {
         cout << "Y Has Won";
     }
 
